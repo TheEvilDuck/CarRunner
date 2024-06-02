a548478d2e88538991bb6213f314d302349205e7
TheEvilDuck <boxheaddyamonag@gmail.com>
2024-06-03 03:30:49 +1000

    LevelData to Level
    Переделал название класса

--- Assets/Levels/Scripts/Level.cs
+++ Assets/Levels/Scripts/Level.cs
@@ -1,20 +1,28 @@
 using System.Collections.Generic;
+using Barmetler.RoadSystem;
+using Gameplay;
 using Gameplay.Garages;
 using Gameplay.TimerGates;
 using UnityEngine;
 
-namespace Gameplay.Levels
+namespace Levels
 {
-    public class LevelData : MonoBehaviour
+    public class Level : MonoBehaviour
     {
         [SerializeField] private List<Garage> _garages;
         [SerializeField] private List<TimerGate> _timerGates;
         [SerializeField] private SimpleCarCollisionTrigger _finish;
         [SerializeField] private Transform _carStartPosition;
+        [SerializeField] private RoadMeshGenerator _roadMeshGenerator;
 
         public IEnumerable<Garage> Garages => _garages;
         public IEnumerable<TimerGate> TimerGates => _timerGates;
         public SimpleCarCollisionTrigger Finish => _finish;
         public Vector3 CarStartPosition => _carStartPosition.position;
+
+        private void Awake() 
+        {
+            _roadMeshGenerator.GenerateRoadMesh();
+        }
     }
 }
