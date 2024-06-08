6547e29f58ba080d6fe698b4bd57c510a3e851c4
TheEvilDuck <boxheaddyamonag@gmail.com>
2024-06-08 17:38:33 +1000

    Pause manager
    Добавил сущность, которая в себе списком хранит все, что нужно запаузить. С ней и будем взаимодействовать

---
+++ Assets/Common/PauseManager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+namespace Common
+{
+    public class PauseManager: IPausable
+    {
+        private List<IPausable> _pausables;
+
+        public PauseManager()
+        {
+            _pausables = new List<IPausable>();
+        }
+
+        public void Register(IPausable pausable)
+        {
+            if (pausable == this)
+                throw new ArgumentException("Pause manager must not contain itself");
+        }
+
+        public void Pause()
+        {
+            foreach (IPausable pausable in _pausables)
+            {
+                pausable.Pause();
+            }
+        }
+
+        public void Resume()
+        {
+            foreach (IPausable pausable in _pausables)
+            {
+                pausable.Resume();
+            }
+        }
+    }
+}
