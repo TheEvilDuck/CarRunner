c2a4e16dccfb439466ac7ce731437c8e50912137
TheEvilDuck <boxheaddyamonag@gmail.com>
2024-06-28 20:29:17 +1000

    Camera follow update
    -В кои-то веки удалось камеру нормально сделать. Она теперь поворачивается но направлению фактического движения машины, а во избежание подергиваний есть пороговые значения по скорости и углу (сильно большие не ставить, иначе будет ступенчато поворачиваться). Таким образом, машина больше не улетает из поля зрения при заносе, мы ее видим.

--- Assets/Common/CameraFollow/CameraFollow.cs
+++ Assets/Common/CameraFollow/CameraFollow.cs
@@ -7,12 +7,18 @@ namespace Common.Components
     [SerializeField] private Transform _target;
     [SerializeField] private Vector3 _offset;
     [SerializeField] private Vector3 _lookAtOffset;
+    [SerializeField, Min(0)] private float _minTargetVelocity;
+    [SerializeField, Min(0)] private float _minVelocityAngle;
     [SerializeField, Range(0, 1f)] private float _speed;
+
+    private Vector3 _targetLastPosition;
+    private Vector3 _lastVelocity;
     
 
     public void SetTarget(Transform target)
     {
         _target = target;
+        _targetLastPosition = _target.position;
     }
 
     private void Update() 
@@ -20,15 +26,29 @@ namespace Common.Components
         if (_target == null)
             return;
 
-        var targetPosition = _target.position + _target.TransformDirection(_offset);
-        var moveVector = targetPosition - transform.position;
+        Vector3 moveVector = _target.position - _targetLastPosition;
+        float angle = Vector3.SignedAngle(moveVector.normalized, Vector3.forward, Vector3.up);
 
-        var interpolated = Vector3.Lerp(transform.position, targetPosition, _speed * moveVector.magnitude * Time.deltaTime);
-        transform.position = interpolated;
+        Vector3 offset = Quaternion.AngleAxis(-angle, Vector3.up) * _offset;
+
+        if (moveVector.sqrMagnitude < Mathf.Pow(_minTargetVelocity, 2) || Mathf.Abs(angle) < _minVelocityAngle)
+        {
+            angle = Vector3.SignedAngle(_lastVelocity.normalized, Vector3.forward, Vector3.up);
+            offset = Quaternion.AngleAxis(-angle, Vector3.up) * _offset;
+        }
+        else
+        {
+            _lastVelocity = moveVector;
+        }
+
+        Vector3 goal =  _target.position + offset;
+        float speed = (goal - transform.position).magnitude * _speed * Time.deltaTime;
+        Vector3 goalLerp = Vector3.LerpUnclamped(transform.position, goal, speed);
+
+        transform.position = goalLerp;
+        transform.LookAt(_target.position);
         
-        transform.LookAt(_target.position + _target.TransformDirection(_lookAtOffset));
-        
-        
+        _targetLastPosition = _target.position;
     }
 }
 }
