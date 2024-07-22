ae88cb873662d2abb3ff6a0ab8b36be6b9fafd27
TheEvilDuck <boxheaddyamonag@gmail.com>
2024-07-22 18:35:34 +1000

    Tags in DI container
    Добавил поддержку строковых тегов для DI контейнера

--- Assets/DI/DIContainer.cs
+++ Assets/DI/DIContainer.cs
@@ -6,44 +6,50 @@ namespace DI
     public class DIContainer
     {
         private readonly DIContainer _parent;
-        private readonly Dictionary<Type, ObjectData> _objects;
-        private readonly HashSet<Type> _cyclicCash;
+        private readonly Dictionary<(string, Type), ObjectData> _objects;
+        private readonly HashSet<(string, Type)> _cyclicCash;
 
         public DIContainer(DIContainer parent = null)
         {
-            _objects = new Dictionary<Type, ObjectData>();
-            _cyclicCash = new HashSet<Type>();
+            _objects = new Dictionary<(string, Type), ObjectData>();
+            _cyclicCash = new HashSet<(string, Type)>();
             _parent = parent;
         }
 
-        public void Register<T>(Func<T> createFunc)
+        public void Register<T>(Func<T> createFunc, string tag = "")
         {
-            if (_objects.ContainsKey(typeof(T)))
-                throw new ArgumentException($"There is already registered object with type of {typeof(T)}");
+            var tupple = (tag, typeof(T));
+
+            if (_objects.ContainsKey(tupple))
+                throw new ArgumentException($"There is already registered object with type of {typeof(T)} and tag {tag}");
 
             var objectData = new ObjectData<T>(createFunc);
-            _objects.Add(typeof(T), objectData);
+            _objects.Add(tupple, objectData);
         }
 
-        public void Register<T>(T value)
+        public void Register<T>(T value, string tag = "")
         {
-            if (_objects.ContainsKey(typeof(T)))
-                throw new ArgumentException($"There is already registered object with type of {typeof(T)}");
+            var tupple = (tag, typeof(T));
+
+            if (_objects.ContainsKey(tupple))
+                throw new ArgumentException($"There is already registered object with type of {typeof(T)} and tag {tag}");
 
             var objectData = new ObjectData<T>(value);
-            _objects.Add(typeof(T), objectData);
+            _objects.Add(tupple, objectData);
         }
 
-        public T Get<T>()
+        public T Get<T>(string tag = "")
         {
-            if (_cyclicCash.Contains(typeof(T)))
-                throw new Exception($"Cyclic dependency accured with type of {typeof(T)}");
+            var tupple = (tag, typeof(T));
 
-            _cyclicCash.Add(typeof(T));
+            if (_cyclicCash.Contains(tupple))
+                throw new Exception($"Cyclic dependency accured with type of {tupple} and tag {tag}");
+
+            _cyclicCash.Add(tupple);
 
             try
             {
-                if (_objects.TryGetValue(typeof(T), out var objectData))
+                if (_objects.TryGetValue(tupple, out var objectData))
                     return objectData.Get<T>();
 
                 if (_parent != null)
@@ -51,10 +57,10 @@ namespace DI
             }
             finally
             {
-                _cyclicCash.Remove(typeof(T));
+                _cyclicCash.Remove(tupple);
             }
 
-            throw new Exception($"DI container doesn't know how to create {typeof(T)}, do you forget to register entity?");
+            throw new Exception($"DI container doesn't know how to create {tupple} and tag {tag}, do you forget to register entity?");
         }
     }
 }
