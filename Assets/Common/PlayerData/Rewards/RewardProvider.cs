fa5e20e5a5d457a676fc58c5fe9a7c4bc24de67c
TheEvilDuck <boxheaddyamonag@gmail.com>
2024-08-07 20:03:09 +1000

    First visual test
    -Добавил тестово уровень
    -Сделал ему скайбокс
    -Наимпортировал моделек

--- Assets/Common/PlayerData/Rewards/RewardProvider.cs
+++ Assets/Common/PlayerData/Rewards/RewardProvider.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Levels;
 using UnityEngine;
 
@@ -7,6 +8,7 @@ namespace Common.Data.Rewards
     {
         private const int TUTORIAL_COMPLETION_REWARD = 1000;
 
+        private const float COINS_MULTIPLIER_FOR_REPLAYING_LEVEL = 0.25f;
         public int GetLevelCompletionReward(float remainingTime, IPlayerData playerData, LevelsDatabase levelsDatabase)
         {
             string nextLevelId = levelsDatabase.GetNextLevelId(playerData.SelectedLevel);
@@ -19,8 +21,13 @@ namespace Common.Data.Rewards
             foreach (var timerGate in level.TimerGates)
                 if (timerGate.Time > 0)
                     sumOfTimerGates += timerGate.Time;
+
+            float k = 1f;
+
+            if (playerData.PassedLevels.Contains(playerData.SelectedLevel))
+                k = COINS_MULTIPLIER_FOR_REPLAYING_LEVEL;
             
-            int coins = Mathf.CeilToInt(nextLevelCost * (remainingTime / (startTime + sumOfTimerGates)) * rewardDivider);
+            int coins = Mathf.CeilToInt(nextLevelCost * (remainingTime / (startTime + sumOfTimerGates)) * rewardDivider * k);
             return coins;
         }
 
