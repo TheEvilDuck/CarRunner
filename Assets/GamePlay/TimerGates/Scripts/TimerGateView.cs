83d400114840506b7a8a3ef18c50cf13f1b25786
TheEvilDuck <boxheaddyamonag@gmail.com>
2024-08-10 01:39:40 +1000

    Update assets
    -Переместил ассеты в папку ресурсов
    -Обновил ассеты дорог

--- Assets/GamePlay/TimerGates/Scripts/TimerGateView.cs
+++ Assets/GamePlay/TimerGates/Scripts/TimerGateView.cs
@@ -1,3 +1,4 @@
+using Common.UI.UIAnimations;
 using TMPro;
 using UnityEngine;
 
@@ -6,13 +7,41 @@ namespace Gameplay.TimerGates
     public class TimerGateView : MonoBehaviour
     {
         [SerializeField]private TextMeshProUGUI _text;
+        [SerializeField] private Renderer _renderer;
+        [SerializeField] private GameObject _midPart;
+        [SerializeField] private Color32 _goodColor;
+        [SerializeField] private Color32 _badColor;
+        [SerializeField] private UIAnimatorSequence _textAnimator;
+        private TimerGate _timerGate;
 
-        public void Init(float time)
+        public void Init(TimerGate timerGate)
         {
-            if (time>=0)
-                _text.text = $"+{time}";
+            _timerGate = timerGate;
+            MaterialPropertyBlock materialPropertyBlock = new MaterialPropertyBlock();
+
+            if (_timerGate.Time>=0)
+            {
+                _text.text = $"+{_timerGate.Time}";
+                _renderer.material.SetColor("_TintColor", _goodColor);
+            }
             else
-                _text.text = time.ToString();
+            {
+                _text.text = _timerGate.Time.ToString();
+                _renderer.material.SetColor("_TintColor", _badColor);
+            }
+
+            _timerGate.passed += OnPassed;
+        }
+
+        private void OnDestroy() 
+        {
+            _timerGate.passed -= OnPassed;
+        }
+
+        private void OnPassed(float time)
+        {
+            _midPart.SetActive(false);
+            _textAnimator.StartSequence();
         }
     }
 }
