3f17f00929669b50009d72a3b66525ebf7a8a7c6
TheEvilDuck <boxheaddyamonag@gmail.com>
2024-08-25 01:53:55 +1000

    Fix
    -Пофиксил ошибки аниматора своего, при которых курутина пыталась работать на выключенных объектах.

--- Assets/Common/UI/Scripts/UIAnimatorSequence.cs
+++ Assets/Common/UI/Scripts/UIAnimatorSequence.cs
@@ -66,7 +66,7 @@ namespace Common.UI.UIAnimations
 
             if (_currentAnimation < _animations.Count)
             {
-                if (_animations[_currentAnimation].animator.enabled)
+                if (_animations[_currentAnimation].animator.enabled && _animations[_currentAnimation].animator.isActiveAndEnabled)
                     StartCoroutine(PlayAnimation());
                 else
                     PlayNext();
