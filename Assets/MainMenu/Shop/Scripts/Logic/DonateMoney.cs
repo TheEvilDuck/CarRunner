91a0441d72da2f6391bf33963c4ba9a2beda02fc
Salavat <salavatfattahov@gmail.com>
2024-08-30 15:20:29 +0500

    Связал покупки в магазине с покупками ЯндексSDK, а так-же добавил проверку на неиспользованные покупки

--- Assets/MainMenu/Shop/Scripts/Logic/DonateMoney.cs
+++ Assets/MainMenu/Shop/Scripts/Logic/DonateMoney.cs
@@ -1,26 +1,50 @@
 using Common.Data;
 using DI;
 using UnityEngine;
+using YG;
 
 namespace MainMenu.Shop.Logic
 {
     [CreateAssetMenu(menuName = "Shop/Shop items/new donate money", fileName = "Donate money")]
     public class DonateMoney : ShopItem
     {
-        [field: SerializeField, Min(0)] public int Cost {get; private set;}
-        [field: SerializeField, Min(0)] public int CoinsReward {get; private set;}
+        [field: SerializeField] public string Id { get; private set; }
+        [field: SerializeField, Min(0)] public int Cost { get; private set; }
+        [field: SerializeField, Min(0)] public int CoinsReward { get; private set; }
+
+        public override void Init(DIContainer sceneContext)
+        {
+            void onPurchaseSuccessEvent(string id)
+            {
+                YandexGame.PurchaseSuccessEvent -= onPurchaseSuccessEvent;
+
+                if (id == Id)
+                {
+                    sceneContext.Get<IPlayerData>().AddCoins(CoinsReward);
+                    Claim();
+                }
+            }
+
+            YandexGame.PurchaseSuccessEvent += onPurchaseSuccessEvent;
+            YandexGame.ConsumePurchases();
+        }
 
         public override bool TryClaim(DIContainer sceneContext)
         {
-            //TODO replace this true to SDK check for yandex coins
-            if (true)
+            void onPurchaseSuccessEvent(string id)
             {
-                sceneContext.Get<IPlayerData>().AddCoins(CoinsReward);
-                Claim();
-                return true;
+                YandexGame.PurchaseSuccessEvent -= onPurchaseSuccessEvent;
+
+                if (id == Id)
+                {
+                    sceneContext.Get<IPlayerData>().AddCoins(CoinsReward);
+                    Claim();
+                }
             }
 
-            return false;
+            YandexGame.PurchaseSuccessEvent += onPurchaseSuccessEvent;
+            YandexGame.BuyPayments(Id);
+            return true;
         }
     }
 }
