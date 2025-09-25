b8763f792ea832aeb21c1068957125286609091f
TheEvilDuck <boxheaddyamonag@gmail.com>
2025-09-26 02:49:17 +1000

    End game menu fix
    -Там тоже кнопки не запускали смену сцены

--- Assets/GamePlay/Mediators/EndGameMediator.cs
+++ Assets/GamePlay/Mediators/EndGameMediator.cs
@@ -1,5 +1,6 @@
 using System;
 using Common;
+using Common.CoroutinePerformer;
 using GamePlay.UI.Scripts;
 using Infrastructure.DI;
 using Services.SceneManagement;
@@ -11,12 +12,14 @@ namespace GamePlay.Mediators
         private readonly EndOfTheGame _endGameUI;
         private readonly PauseManager _pauseManager;
         private readonly ISceneManager _sceneManager;
+        private readonly ICoroutinePerformer _coroutinePerformer;
 
         public EndGameMediator(IDIContainer sceneContext)
         {
             _endGameUI = sceneContext.Get<EndOfTheGame>();
             _pauseManager = sceneContext.Get<PauseManager>();
             _sceneManager = sceneContext.Get<ISceneManager>();
+            _coroutinePerformer = sceneContext.Get<ICoroutinePerformer>();
 
             _endGameUI.SceneChangingButtons.RestartButtonPressed.AddListener(RestartLevel);
             _endGameUI.SceneChangingButtons.GoToMainMenuButtonPressed.AddListener(LoadMainMenu);
@@ -31,13 +34,13 @@ namespace GamePlay.Mediators
         private void RestartLevel()
         {
             _pauseManager.Unlock();
-            _sceneManager.LoadScene(SceneIDs.GAMEPLAY);
+            _coroutinePerformer.StartCoroutine(_sceneManager.LoadScene(SceneIDs.GAMEPLAY));
         }
 
         private void LoadMainMenu()
         {
             _pauseManager.Unlock();
-            _sceneManager.LoadScene(SceneIDs.MAIN_MENU);
+            _coroutinePerformer.StartCoroutine(_sceneManager.LoadScene(SceneIDs.MAIN_MENU));
         }
     }
 }
